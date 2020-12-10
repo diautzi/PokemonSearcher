ac8fb52616494d2e06542d7202ec7954b4c3f61b
Diana Utan <didiutzi@gmail.com>
2020-12-10 17:11:31 -0600

    first commit

---
+++ src/components/PokemonCollection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PokemonCollection() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PokemonCollection
---
+++ src/components/PokemonIndex.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Search } from 'semantic-ui-react';
+
+
+export default function PokemonIndex() {
+    return (
+        <div>
+            <h1>Pokemon Searcher</h1>
+            <Search />
+        </div>
+    )
+}
+
+export default 
